927ef93e58156a823cc795eae7c2ab904a04f865
finally1818 <https://gitee.com/finally1818>
2020-01-08 23:37:11 +0800

    fix:修改webpack生产和开发配置

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -6,36 +6,36 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const uglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = WebpackMerge(webpackConfig, {
-  // mode: 'production',
-  // devtool: 'cheap-module-source-map',
-  // plugins: [
-  //   new CopyWebpackPlugin([
-  //     {
-  //       form: path.resolve(__dirname, '../public'),
-  //       to: path.resolve(__dirname, '../dist')
-  //     }
-  //   ])
-  // ],
-  // optimization: {
-  //   minimizer: [
-  //     newUglifyJsPlugin({
-  //       // 压缩js
-  //       cache: true,
-  //       parallel: true,
-  //       sourceMap: true
-  //     }),
-  //     new OptimizeCssAseetsPlugin({})
-  //   ],
-  //   splitChunks: {
-  //     chunks: 'all',
-  //     cacheGroups: {
-  //       libs: {
-  //         name: 'chunk-libs',
-  //         test: /[\\/]node_modules[\\/]/,
-  //         priority: 10,
-  //         chunks: 'initial' // 只打包初始时以来的第三方
-  //       }
-  //     }
-  //   }
-  // }
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        form: path.resolve(__dirname, '../public'),
+        to: path.resolve(__dirname, '../dist'),
+      },
+    ]),
+  ],
+  optimization: {
+    minimizer: [
+      newUglifyJsPlugin({
+        // 压缩js
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCssAseetsPlugin({}),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: 'initial', // 只打包初始时以来的第三方
+        },
+      },
+    },
+  },
 })
